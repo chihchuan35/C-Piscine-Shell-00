f1442dacf5edca14b9977c8d54fb9f6815ccbe5c
chihchuan35 <150209346+chihchuan35@users.noreply.github.com>
2025-01-20 10:55:05 +1030

    Create z.c

---
+++ z.c
@@ -0,0 +1,5 @@
+$ git clone --bare git@vogsphere.42adel.org.au:vogsphere/intra-uuid-442350ea-dfdd-4577-ab97-5ba7fea93c1c-6284620-chcheng
+$ cd REPO.git
+$ git push --mirror git@vogsphere.42adel.org.au:vogsphere/intra-uuid-442350ea-dfdd-4577-ab97-5ba7fea93c1c-6284620-chcheng
+cd ..
+rm -rf REPO.git
